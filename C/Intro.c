5cb53f7a26b07a5712f7489b11eefefedf44957f
Tahsin Emre Öztürk <tahsinemrre@gmail.com>
2025-03-18 04:21:09 +0300

    2.5

--- C/Intro.c
+++ C/Intro.c
@@ -1,11 +1,85 @@
-//#include <stdio.h>
-//
-//int main (void)
-//{
-//	printf("hello world\n");
-//
-//	printf("Welcome\nto\nC!\n");
-//
-//
-//	return 0;
-//}
+// C programlamada ilk kod örneði ve ilgili konu hakkýnda bilgiler.
+
+
+#include <stdio.h>
+
+int main (void)
+{
+	printf("hello world\n");
+
+	printf("Welcome\nto\nC!\n");
+
+
+	return 0;
+}
+
+// C de Yorum satiri ===> //
+// Yorum satýrý C derleyicisi tarafýndan göz ardý edilir
+// içerisindeki ifadeler makine-dili nesne kodu üretilmesine neden olmaz.
+
+// Çok satýrlý yorum satýrý için /*...*/ kullanýlýr. Fakat bu kullaným yaygýn programlama ha-
+//talarýna neden olabilir. Çünkü */ ile kapatýlmasý unutulur. Bu hatayý önlemek için // avantajlýdýr.
+
+/*
+	#include ÖN ÝÞLEMCÝ TALÝMATI
+	# ile baþlayan satýlar derlemeden önce ön iþlemci tarafýndan iþlenir. Örnek olarak 
+	#include <stdio.h> kodu ile standart giriþ/çýkýþ yani (<stdio.h>) özelliðini programa 
+	eklemesini söyler. Yani printf gibi fonksiyonlar çaðýrýldýklarýnda kullanýlan bilgiler
+	bu ön iþlemci talimatý ile saðlanýr.
+
+	BOÞ SATIRLAR
+	Programý daha kolay okumak için boþ satýrlar býrakýlýr. Boþluk karekteri ve sekme karek-
+	terleri kullanýlarak yapýlýr. Bu karekterlerin hepsi beyaz boþluk olarak adlandýrýlýr ve
+	normalde derleyici tarafýndan göz ardý edilirler.
+
+	MAIN FONSKÝYONU
+	C programýnýn yapý taþýdýr. Her programda olmak zorundadýr. C programlarý yürütülmeye 
+	main fonksiyonu ile baþlar. Fonksiyonlar bilgi gönderebilir. Yani int main () {} gibi. Bu int
+	ifadesi main fonksiyonun bir tam sayý döndereceði anlamýna gelir. 
+	Ayný zamanda fonksiyonlar yürütülmek için çaðýrýldýðýnda veri de alabilirler. Mesela
+	int main (void) { } fonksiyonunda void ifadesi bu fonkisyonun hiçbir veri almadýðý anlamýna 
+	gelir.
+	--> Not:Her fonksiyon kendisinden önce fonksiyonun amacýný tanýmlayan bir açýklamaya sahip olmalýdýr.
+
+
+	ÇIKIÞ ÝFADESÝ
+	printf (" "); ifadesi, bilgsayara ifadeleri ekrana bastýrmak için talimat verir. printf fonksiyonu
+	parantez içindeki argümaný ve noktalý virgülü dahil tüm satýra ifade denir. Her ifade de 
+	noktalý virgül ; ile bitirilmek zorundadýr.
+
+
+
+	KAÇIÞ DÝZÝNLERÝ
+	mesela \n karekteri printfde yazýlmasýna raðmen ekrana basýlmaz. Bunun sebebi \ ile bir kaçýþ
+	iþlemi olmasýdýr. Yani bir dizi içinde ters bölü iþareti algýlandýðýnda, ters bölüden sonraki
+	karaktere bakar ve ters bölü ile birleþtirerek bir kaçýþ ifadesi haline gelir.
+
+	\n  Yeni satýr. Ýmleci sonraki satýr baþýna konumlandýrýr.  
+
+	\t  Yatak sekme. Ýmleci sonraki sekme noktasýna kaydýrýr.
+
+	\a  Uyarý. Ýmlecin o anki komunu deðiþtirmeden, görsel veya ses olarak uyarý üretir.
+
+	\\  Ters bölüyü ekrana bastýrmak için bu þekilde kullanýlýr.
+
+	\"  Çift týrnak ekrana basýtýrýlmak için bu þekilde kullanýlýr.
+
+
+	BAÐLAYICI VE YÜRÜTÜLEBÝLÝRLER
+	Baðlayýcý (Linker)	Nesne dosyalarýný birleþtirerek çalýþtýrýlabilir dosya üretir.
+	Yürütülebilir (Executable)	Ýþletim sisteminin çalýþtýrabileceði makine kodu dosyasýdýr.
+	Baðlayýcý, derleme sürecinin son aþamasýnda çalýþan bir araçtýr. 
+	Temel görevi, derlenmiþ nesne dosyalarýný (.o veya .obj dosyalarý) birleþtirerek tek bir
+	yürütülebilir (.exe, .out vb.) dosya oluþturmaktýr.
+	Yürütülebilir dosya, çalýþtýrýlabilir bir programdýr. Baðlayýcý tarafýndan oluþturulan bu dosya,
+	iþletim sistemi tarafýndan çalýþtýrýlabilir.
+
+
+
+
+
+
+
+
+
+*/
--- TwoSum/TwoSum.c
+++ TwoSum/TwoSum.c
@@ -7,6 +7,8 @@ int main()
 
 	int sum;
 
+	//int integer1, integer2, sum;  Bu þekilde de kullanýlabilir.
+
 	printf("Enter first integer\n");
 	scanf_s("%d", &integer1);
 
@@ -19,3 +21,64 @@ int main()
 
 	return 0;
 }
+
+
+/*
+	DEÐÝÞKEN VE DEÐÝÞKEN TANIMLAMALARI
+	Deðiþlenler bir program kullanýmý için hafýzada saklanan yerlerdir. Bu deðiþlenler taným-
+	lanýrken türü ve ismi belirtilir. Örneðin int, string gibi.
+
+
+	KÝMLÝKLER VE BÜYÜK KÜÇÜK HARF DUYARLILIÐI
+	C de bir deðiþken ismi ayný zamanda bir kimliktir. Bir kimlik sayý ile baþlamaz. Harf
+	sayý ve alt çizgi _ den oluþan karekter dizisidir. 
+	C büyük küçük harfe duyarlýdýr. Örneðin a1 ile A1 C de farklý kimliklerdir.
+	---> Not: kimlikler küçük harfle baþlamalýdýr. Büyük harfle baþlayan ya da hepsi büyük 
+	olanlarýn farklý bir anlamý vardýr.
+	---> Not2: Derleyici tarafýndan oluþturulan kimlikler ile standart kütüphanelerde tanýmlý
+	olan kimliklerin çakýþmasýný önlemek için _ alt çizgi ile baþlayan kimliklerden sakýnýn.
+
+	---> Not3: deðiþken isimlerinin birden fazla kelimeden oluþtuðunda diðer kelimelerin ilk harfleri
+	büyük olmalýdýr. forExample gibi.
+
+	---> Not4: Deðiþken tanýmlamlarýný yürütülebilir ifadelerin arasýna yazmak söz dizim 
+	hatasýna neden olabilir.
+
+
+	ÝSTEM MESAJLARI
+	printf("Enter first integer\n"); ifadesinde olduðu gibi kullanýcýdan bir eylem yapmasýný
+	isteyen ifadelere istem denir.
+
+
+	SCANF FONKSÝYONU VE BÝÇÝMLENDÝRÝLMÝÞ GÝRÝÞLER
+	scanf_s("%d", &integer1); kullanýcýdan bir deðer almak için scanf kullanýlýr. Bu fonksiyon
+	genelde standart giriþ birimi olan klavyeden okur. Scanf'in %d ve &integer1 gibi iki argü-
+	maný vardýr. Birincisi %d kullanýcýdan alýnacak verinin türünü, ikincisi & (asemptont) da
+	scanf' e deðiþkenin bellekteki yerini söyler. Sonrasýnda da kullanýcýndan alýnan veri deðiþ-
+	kenin bellekteki yerine yazýlýr. 
+	Bu etkileþime Etkileþimli Hesaplama denir.
+
+
+
+
+	ATAMA ÝFADELERÝ
+	sum = integer1 + integer2; Çoðu hesaplama atama iþleminde icra edilir. Saðdan sola doðru
+	atama olur. Önce integer1 ve integer2 toplanýr, arýndan sum deðiþkenine bu toplamýn deðeri
+	atanmýþ olur. 
+
+
+
+	BÝÇÝM KONTROL KARAKTER KATARI ÝLE YAZMA
+	printf("Sum is %d\n", sum); Burada printf' in iki argümaný vardýr. Birincisi %d ikincisi de
+	virgülden sonraki sum. %d ile ekrana yazdýrýlacak deðerin türünü ikinci argüman ile de bu
+	verinin hangisi olduðudur.
+
+
+	PRINTF ÝFADELERÝ HESAPLAMALAR
+	Hesaplamalar ayný zamanda printf içinde de gerçekleþtirilebilir.
+	printf ("sum is %d\n", integer1 + integer2 ); gibi.
+
+
+
+
+*/
