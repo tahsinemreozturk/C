2a21fa50ba11f4c9b2d2fb154105512e7d8bd689
Tahsin Emre Öztürk <tahsinemrre@gmail.com>
2025-03-20 16:51:58 +0300

    Exercies2.24

---
+++ Part2Exercises/2.24.c
@@ -0,0 +1,22 @@
+//#include <stdio.h>
+//
+//int main(void) 
+//{
+//	printf("This program calculates whether the entered number is even or odd.\n");
+//
+//	int num1, result;
+//	printf("Enter an integer: \n");
+//	scanf_s("%d", &num1);
+//
+//	result = num1 % 2;
+//
+//	if (result != 0)
+//	{
+//		printf("The Number Is Odd \n");
+//	}
+//	if (result == 0)
+//	{
+//		printf("The Number Is Even\n");
+//	}
+//
+//}
