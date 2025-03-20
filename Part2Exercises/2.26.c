255fc47c024665e7979b7ba771f1aa76546f1044
Tahsin Emre Öztürk <tahsinemrre@gmail.com>
2025-03-20 17:19:03 +0300

    Exercies2.27

--- Part2Exercises/2.26.c
+++ Part2Exercises/2.26.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
-
-int main (void)
-{
-	printf("This program calculates whether the 1st number entered is a multiple of the 2nd.\n");
-
-	int num1, num2, result;
-
-	printf("Enter Two Integers: \n");
-	scanf_s("%d%d", &num1, &num2);
-
-	result = num2 % num1;
-
-	if (result != 0)
-	{
-		printf("Number 1st is not a multiple of number 2nd\n");
-	}
-
-	if( result == 0) 
-	{
-		printf("Number 1st is a multiple of number 2nd\n");
-	}
-}
+//#include <stdio.h>
+//
+//int main (void)
+//{
+//	printf("This program calculates whether the 1st number entered is a multiple of the 2nd.\n");
+//
+//	int num1, num2, result;
+//
+//	printf("Enter Two Integers: \n");
+//	scanf_s("%d%d", &num1, &num2);
+//
+//	result = num2 % num1;
+//
+//	if (result != 0)
+//	{
+//		printf("Number 1st is not a multiple of number 2nd\n");
+//	}
+//
+//	if( result == 0) 
+//	{
+//		printf("Number 1st is a multiple of number 2nd\n");
+//	}
+//}
---
+++ Part2Exercises/2.27.c
@@ -0,0 +1,23 @@
+//#include <stdio.h>
+//
+//int main (void)
+//{
+//	printf("Version 1\n\n");
+//	printf("* * * * * * * *\n");
+//	printf(" * * * * * * * *\n");
+//	printf("* * * * * * * *\n");
+//	printf(" * * * * * * * *\n");
+//	printf("* * * * * * * *\n");
+//	printf(" * * * * * * * *\n");
+//	printf("* * * * * * * *\n");
+//	printf(" * * * * * * * *\n\n\n\n");
+//
+//	printf("Version 2\n\n");
+//	printf("* * * * * * * *\n * * * * * * * *\n");
+//	printf("* * * * * * * *\n * * * * * * * *\n");
+//	printf("* * * * * * * *\n * * * * * * * *\n");
+//	printf("* * * * * * * *\n * * * * * * * *\n\n\n\n");
+//
+//	printf("Version 3\n\n");
+//	printf("* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n");
+//}
