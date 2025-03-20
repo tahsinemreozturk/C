d4ea979a1bc4570fcab482c13a8a2268ba06c987
Tahsin Emre Öztürk <tahsinemrre@gmail.com>
2025-03-20 17:34:41 +0300

    Exercies2.29

---
+++ Part2Exercises/2.29.c
@@ -0,0 +1,20 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("A`nin C deki tam sayi degeri: %d\n", 'A');
+//	printf("B`nin C deki tam sayi degeri: %d\n", 'B');
+//	printf("C`nin C deki tam sayi degeri: %d\n", 'C');
+//	printf("a`nin C deki tam sayi degeri: %d\n", 'a');
+//	printf("b`nin C deki tam sayi degeri: %d\n", 'b');
+//	printf("c`nin C deki tam sayi degeri: %d\n", 'c');
+//	printf("0`in C deki tam sayi degeri: %d\n", '0');
+//	printf("1`in C deki tam sayi degeri: %d\n", '1');
+//	printf("2'nin C deki tam sayi degeri: %d\n", '2');
+//	printf("$`in C deki tam sayi degeri: %d\n", '$');
+//	printf("*`in C deki tam sayi degeri: %d\n", '*');
+//	printf("+`nin C deki tam sayi degeri: %d\n", '+');
+//	printf("/`nin C deki tam sayi degeri: %d\n", '/');
+//	printf(" `un C deki tam sayi degeri: %d\n", ' ');
+//
+//}
