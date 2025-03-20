81c209653da5d0ffb56d2167cf01ff5d9768e72b
Tahsin Emre Öztürk <tahsinemrre@gmail.com>
2025-03-20 03:07:21 +0300

    Exercise2.5

--- Part2Exercises/2.3.c
+++ Part2Exercises/2.3.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
-
-int main(void) 
-{
-	int c, thisVariable, q76354, number;
-
-	printf("Please Enter An Integer: \n");
-	scanf_s("%d", &number);
-
-	printf("Enter An Integer For The a Value \n");
-	int a;
-	scanf_s("%d", &a);
-	printf("a:%d\n", a);
-
-	if (number != 7) {
-		printf("The variable number is not 7.\n\n\n\n");
-	}
-
-	printf("This is a C program.\n\n\n");
-	printf("This is a C\nprogram.\n\n\n");
-	printf("This\nis\na\nC\nprogram.\n\n\n");
-	printf("This\tis\ta\tC\tprogram.\n\n\n");
-
-	return 0;
-
-}
+//#include <stdio.h>
+//
+//int main(void) 
+//{
+//	int c, thisVariable, q76354, number;
+//
+//	printf("Please Enter An Integer: \n");
+//	scanf_s("%d", &number);
+//
+//	printf("Enter An Integer For The a Value \n");
+//	int a;
+//	scanf_s("%d", &a);
+//	printf("a:%d\n", a);
+//
+//	if (number != 7) {
+//		printf("The variable number is not 7.\n\n\n\n");
+//	}
+//
+//	printf("This is a C program.\n\n\n");
+//	printf("This is a C\nprogram.\n\n\n");
+//	printf("This\nis\na\nC\nprogram.\n\n\n");
+//	printf("This\tis\ta\tC\tprogram.\n\n\n");
+//
+//	return 0;
+//
+//}
