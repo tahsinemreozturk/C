195da75a6102933ec8cfc4f7d303fe5cc02a84af
Tahsin Emre Öztürk <tahsinemrre@gmail.com>
2025-03-26 18:54:54 +0300

    3.1Intro

---
+++ AlgorithmsAndPseudocode/AlgorithmsAndPseudocode.c
@@ -0,0 +1,29 @@
+/*
+Belirli bir problemi çözmek için programý yazmaya baþlamadan önce,
+problemi eksiksiz bir þekilde anlamak ve dikkatli bir þekilde çözümü
+planlamak zorundayýz. 
+
+
+Herhangi bir hesaplama problemine çözüm, belirli bir sýradaki bir dizi
+eylemin yürütülmesiyle olur.
+
+Yürütülecek eylemler ve Bu eylemlerin hangi sýrada yürütüleceði açýsýndan
+problem çözme yöntemine ALGORTÝMA denir.
+Bir bilgisayar programýnda ifadelerin hangi sýrada yürütüleceðini belirlemeye
+Program Kontrolü denir.
+
+
+Sözde Kod: Algoritma geliþtirmemize yardýmcý olan yapay ve gayri resmi bir dildir.
+Dikkatli bir þekilde hazýrlanmýþ sözde kod programý C program karþýlýðýna
+kolaylýkla dönüþütürülebilir. Birçok durumda bu sözde kod ifadeleri C karþýlýklarýyla
+deðiþtirilerek yapýlýr.
+
+
+
+
+
+
+
+
+
+*/
