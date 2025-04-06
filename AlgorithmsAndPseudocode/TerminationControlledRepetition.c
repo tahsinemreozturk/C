20b19c6241566bc4fa43bd0cc48fba26e044d2ca
Tahsin Emre Öztürk <tahsinemrre@gmail.com>
2025-04-06 23:34:14 +0300

    Annotation

---
+++ AlgorithmsAndPseudocode/TerminationControlledRepetition.c
@@ -0,0 +1,42 @@
+/*
+Yukarýdan aþaðý, adým adým düzenleyerek algoritmalarýn formüle 
+edilmesi. 2. Olarak sonlandýrma kontrollü tekrarlama ile 
+yapýlabilir.
+
+Yine bir sýnýfta alýnan notlarýn sýnýf ortalamasýnýn hesaplanacaðý
+program yazýlmasý gereksin. Ama bu sefer kaç tane not girileceðini
+bilmezsek programý nasýl yazabiliriz ?
+Yani tekrarlý iþlemin kaç defa edeceðini önceden bilmediðimiz 
+zamanlarda ne yapabiliriz ? 
+
+Kullanýcýya döngüyü sonlandýran bir deðer bilgisini veririz. Bu 
+sayede kullanýcý tekrarladýðý iþlemin sonuna geldiðinde yani yeni
+bir not girmeyeceði zaman bu deðeri girerek tekrarlamayý sonlandýrýr.
+
+Tabi dikkat edilmesi gereken önemli bir þey de; Sonlandýrma ifadesinin
+normalde girilebilecekl deðerler kümesinde olmamasý gerekir.
+
+Örnek olarak not girilen bir uygulamada, sonlandýrma deðeri 5 seçilirse ve
+girilen notlardan biri de 5 olursa bu sefer program sonlanacak ve 
+istenen iþlem olmasý gerektiði gibi sonuç veremeyecektir. Bu durumlarda
+mantýklý olan notlandýrma içersininde olmayan bir deðeri sonlandýrma
+ifadesi olarak tanýmlamaktýr. Mesela notlarýn (0-100 olduðu bir sýnavda)
+-5 bir sonlandýrma deðeri olarak kullanýlabilir.
+
+Not: Bu tarz durumlara Belirsiz Tekrarlama denir.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
