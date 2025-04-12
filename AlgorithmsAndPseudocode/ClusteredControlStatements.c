4df51e11b8d0a774221309026bccac80fb1e5a77
Tahsin Emre Öztürk <tahsinemrre@gmail.com>
2025-04-12 13:55:29 +0300

    The target problem.

---
+++ AlgorithmsAndPseudocode/ClusteredControlStatements.c
@@ -0,0 +1,26 @@
+/*
+	Aþaðýdaki problem ifadesini göz önüne alalým
+Bir okul, öðrencileri emlakçý sertifika sýnavýna hazýrlayan bir ders açýyor. Geçen
+sene bu dersi tamamlayan öðrencilerden 10' u sertifka sýnavýna girdi. Doðal olarak,
+okul öðrencilerin sýnavý ne kadar iyi yaptýklarýný bilmek istiyor. Sonucu özetleyen
+bir program yazmanýz isteniyor. Bu 10 öðrencinin listesi size verilmiþtir. Her bir
+ismin yanýna eðer öðrenci sýnavý geçtiyse 1 kaldýysa 2 yazýlmýþtýr. 
+
+Programýnýz sýnav sonuçlarýný aþaðýdaki gibi analiz etmelidir:
+1. Her bir sýnav sonucunu girin (örneðin 1 veya 2). Program baþka bir sýnav sonucu
+istediði her sefer "Enter Result" istem mesajýný gösterin.
+
+2. Her tür sýnav sonucunu sayýn
+
+3. Geçen öðrencilerin sayýsýný ve kalanlarýn sayýsýný iþaret eden sýnav sonuçlarýný-
+nýn özetini gösterin.
+
+4. Sekiz öðrenciden fazlasý sýnavý geçtiyse "Bonus to instructor!" mesajýný yazýn.
+
+
+
+
+
+
+
+*/
