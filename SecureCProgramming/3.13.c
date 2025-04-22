700cf200ec150b193cdf9981d854d677aa5e3e8b
Tahsin Emre Öztürk <tahsinemrre@gmail.com>
2025-04-22 23:29:56 +0300

    Security Considerations When Using Unsigned Integers in C Programming.

--- SecureCProgramming/3.13.c
+++ SecureCProgramming/3.13.c
@@ -1,5 +1,5 @@
 /*
-Aritmetik Taþýma 
+ARÝTMETÝK TAÞMA 
 
 sum = integer1 + integer2;
 
@@ -22,6 +22,31 @@ gösterilmektedir. INT32-C Kýlavuzuna bakýnýlabilir.
 Endüstriyel amaçlý kodda, tüm hesaplamalar için bu tür karþýlaþtýrmalarý yapmalýsýnýz.
 Sonraki bölümlerde, böyle hatalarýn üstesinde gelmek için baþka programalama teknik-
 leri göstereceðiz.
+
+
+
+
+ÝÞARETSÝZ TAM SAYILAR
+
+Genelde, yalnýzca negatif olmayan deðerleri saymak için kullanýlmasý gereken sayaçlar
+tam sayý türünden önce unsigned ifadesi ile tanýmlanmalýdýr. unsigned türünden 
+deðiþkenler, 0' dan karþýlýk gelen iþaretli tam sayý türün pozitif aralýðýnýn yaklaþýk
+iki katýna kadar deðerleri temsil edebilir. Ortamýnýzýn en büyük unsigned int deðerini
+<limits.h> ' den UINT_MAX sabitiyle belirlersiniz. (DETAY BÝLGÝ)
+
+Örneðin daha önce sýnýflar ve sýnav notlarý ile ilgili programlar yazmýþtýk. Örneðin
+sýnav notu kullanýcýdan alýrken negatif deðerleri giremesini engellemek için 
+unsigned int kullanabilirdik. Çünkü kullanýclarýn gireceði deðerleri kontrol edeme-
+yeceðimiz için, programlamayý tasarlarken hata paylarýný en aza indirmeyi amaçlama-
+lýyýz. Fakat sonlandýrma kontrollü bir döngüde not deðiþkeninin unsigned olarak
+tanýmlanmasý durumunda, öðrencilerin alabileceði notlar ile, sonlandýrma için giri-
+lecek olan ifade çarpýþabilir. Bu sebeple not için tanýmsýz olan bir ifadeyi sonlan-
+dýrma ifadei olarak ayarlamak gerekir. 
+
+Bu konuda daha fazla bilgi için Robert Seacord' un Secure Coding in C and C++ kitabýnýn
+Integer Security bölümü okunabilir.
+
+
 
 
 
