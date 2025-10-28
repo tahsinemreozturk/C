fac538444273c66830db8f3111ae38ee87a223d7
tahsinemreozturk <tahsinemrre@gmail.com>
2025-10-28 22:26:37 +0300

    Exercise 3.18

---
+++ Part3Exercises/3.18.c
@@ -0,0 +1,31 @@
+
+
+#include <stdio.h>
+
+int main () 
+{
+	printf("$$$ Satis Komisyonu Hesaplayici $$$\n");
+
+	float sabitMaas,satisMiktari, komisyonOrani, komisyonTutari,haftalikMaas;
+
+	komisyonOrani = 0.09;
+	sabitMaas = 200.0;
+
+	printf("Haftalik Satis Miktarinizi Giriniz (-1 cikis icin): ");
+	scanf_s("%f", &satisMiktari);
+	
+	while (satisMiktari != -1)
+	{
+		
+		komisyonTutari = satisMiktari * 0.09;
+		haftalikMaas = sabitMaas + komisyonTutari;
+
+		printf("Haftalik Maasiniz: %.2f TL\n", haftalikMaas);
+
+		printf("Haftalik Satis Miktarinizi Giriniz (-1 cikis icin): ");
+		scanf_s("%f", &satisMiktari);
+	}
+
+	return 0;
+
+}
