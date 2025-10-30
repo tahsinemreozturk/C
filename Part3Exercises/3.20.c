eaaf1a97587e6509d34966bdc59dadca1ad5bac6
tahsinemreozturk <tahsinemrre@gmail.com>
2025-10-30 07:53:43 +0300

    Exercise 3.20

---
+++ Part3Exercises/3.20.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Maas Hesaplayici \n");
+
+	float maas, calismaSaati, saatlikUcreti, primMiktari;
+
+	calismaSaati = 0; // Donguden once bir deger atadik
+
+	printf("Prim miktarinizi giriniz: ");
+	scanf_s("%f", &primMiktari);
+
+	while (calismaSaati != -1)
+	{
+		printf("Iscinin calisma saatini giriniz: ");
+		scanf_s("%f", &calismaSaati);
+
+		printf("Calisanin saatlik ucretini giriniz: ");
+		scanf_s("%f", &saatlikUcreti);
+
+		printf("Calisanin maasi: %.2f \n\n", maas = (calismaSaati * saatlikUcreti) + primMiktari);
+
+	}
+
+	return 0;
+}
