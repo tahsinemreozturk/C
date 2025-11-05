8ff8329316ccc0b753f3ca3cc72abbd0aee4323c
tahsinemreozturk <tahsinemrre@gmail.com>
2025-11-05 08:50:20 +0300

    Exercise 3.25

--- Part3Exercises/3.24.c
+++ Part3Exercises/3.24.c
@@ -1,14 +1,14 @@
-
-#include <stdio.h>
-
-int main()
-{
-	printf("N\t10*N\t100*N\t1000*N\n\n");
-	int i = 1;
-
-	while (i <= 10)
-	{
-		printf("%d\t%d\t%d\t%d\n", i, (i*10),(i*100),(i*1000));
-		i++;
-	}
-}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("N\t10*N\t100*N\t1000*N\n\n");
+//	int i = 1;
+//
+//	while (i <= 10)
+//	{
+//		printf("%d\t%d\t%d\t%d\n", i, (i*10),(i*100),(i*1000));
+//		i++;
+//	}
+//}
