9009bdf232d4d576ff99f36bef3c7d220b397593
tahsinemreozturk <tahsinemrre@gmail.com>
2025-11-06 09:26:46 +0300

    Exercise 3.26

--- Part3Exercises/3.25.c
+++ Part3Exercises/3.25.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-
-int main()
-{
-	int a, i;
-	a = 3;
-	i = 1;
-
-	printf("A\tA+2\tA+4\tA+6\n\n");
-
-	while (i < 6)
-	{
-		printf("%d\t%d\t%d\t%d\n", a, a + 2, a + 4, a + 6);
-		a = a + 3;
-		i++;
-	}
-}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a, i;
+//	a = 3;
+//	i = 1;
+//
+//	printf("A\tA+2\tA+4\tA+6\n\n");
+//
+//	while (i < 6)
+//	{
+//		printf("%d\t%d\t%d\t%d\n", a, a + 2, a + 4, a + 6);
+//		a = a + 3;
+//		i++;
+//	}
+//}
