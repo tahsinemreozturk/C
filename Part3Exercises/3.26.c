2db5ecd7783bcffb4bb593d5690b19e436b049c2
tahsinemreozturk <tahsinemrre@gmail.com>
2025-11-07 11:04:18 +0300

    Exercise 3.27

--- Part3Exercises/3.26.c
+++ Part3Exercises/3.26.c
@@ -1,31 +1,31 @@
-#include <stdio.h>
-
-int main()
-{
-
-	printf("Girilen 10 sayinin en buyuk ikisini bulan program\n");
-	
-	int girilen, buyuk, buyuk2, i;
-	buyuk = buyuk2 = 0;
-
-	i = 0;
-
-	while (i < 10)
-	{
-		printf("Bir pozitif tam sayi giriniz: ");
-		scanf_s("%d", &girilen);
-		i++;
-
-		if (girilen > buyuk)
-		{
-			buyuk2 = buyuk; //i forgot this
-			buyuk = girilen;
-		}
-
-		else if (girilen > buyuk2)
-			buyuk2 = girilen;
-
-	}
-	printf("Girilen en buyuk iki sayi %d ve %d\n", buyuk, buyuk2);
-
-}
+//#include <stdio.h>
+//
+//int main()
+//{
+//
+//	printf("Girilen 10 sayinin en buyuk ikisini bulan program\n");
+//	
+//	int girilen, buyuk, buyuk2, i;
+//	buyuk = buyuk2 = 0;
+//
+//	i = 0;
+//
+//	while (i < 10)
+//	{
+//		printf("Bir pozitif tam sayi giriniz: ");
+//		scanf_s("%d", &girilen);
+//		i++;
+//
+//		if (girilen > buyuk)
+//		{
+//			buyuk2 = buyuk; //i forgot this
+//			buyuk = girilen;
+//		}
+//
+//		else if (girilen > buyuk2)
+//			buyuk2 = girilen;
+//
+//	}
+//	printf("Girilen en buyuk iki sayi %d ve %d\n", buyuk, buyuk2);
+//
+//}
