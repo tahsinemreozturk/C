10b2801c639d0362c9695d062511a1533cf1469a
tahsinemreozturk <tahsinemrre@gmail.com>
2025-11-08 10:00:47 +0300

    Exercise 3.28

---
+++ Part3Exercises/3.28.c
@@ -0,0 +1,32 @@
+﻿//#include <stdio.h>
+//
+//int main(void)
+//{
+//	unsigned int count = 1;
+//
+//	while (count <= 10)
+//	{
+//		puts(count % 2 ? "****" : "++++++++");
+//		++count;
+//	}
+//
+//}
+///*
+//* Buradaki count % 2 kısmı şu şekilde çalışır:
+//
+//Eğer sonuç 1 (veya başka bir sıfır olmayan değer) → true sayılır → "****" seçilir.
+//
+//Eğer sonuç 0 → false sayılır → "++++++++" seçilir.
+//
+//Yani sen “if” gibi açıkça “doğru olursa” demesen bile,
+//C dili otomatik olarak sıfır dışındaki her şeyi doğru kabul ediyor.
+//* 
+//* 
+//* puts(count % 2 ? "****" : "++++++++"); Bu fonksiyon asagidaki gibi
+//* de yapilabilirdi. 
+//* 
+//* if (count % 2)
+//    puts("****");
+//else
+//    puts("++++++++");
+//*/
