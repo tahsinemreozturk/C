c4b8b9f342952d909d1fc57758e5fdb4a1b86c18
tahsinemreozturk <tahsinemrre@gmail.com>
2025-11-10 11:45:55 +0300

    Exercise 3.30

---
+++ Part3Exercises/3.30.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	int x, y;
+	x = 9;
+	y = 11;
+
+	if (x < 10)
+		if (y > 10)
+			puts("*****");
+		else
+			puts("####");
+	puts("$$$$\n\n");
+
+	if (x < 10) {
+		if (y > 10)
+			puts("****");
+	}
+	else {
+		puts("####");
+		puts("$$$$");
+	}
+	// Bu ornek, else ifadesinin hangi if'e baglandigini 
+	// (dangling else problemi) gostermek icin yazilmistir. 
+	// Ýc ice if yapisinda suslu parantez kullanilmazsa 
+	// else en yakin if'e baglanir.
+}
