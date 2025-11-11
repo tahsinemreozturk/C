135db622dc0c9f44862be5df28b5ae9086c0699b
tahsinemreozturk <tahsinemrre@gmail.com>
2025-11-11 11:42:46 +0300

    Exercise 3.31

---
+++ Part3Exercises/3.31.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int x, y;
+	x = 5;
+	y = 8;
+
+	/*
+	
+	/*x = 5 ve y = 8 iken
+
+		@@@@@
+		$$$$$
+		&&&&& cikitisi icin asagidaki kod
+	
+	if (y == 8)
+		if (x == 5)
+			puts("@@@@@");
+		else
+			puts("#####");
+	puts("$$$$$");
+	puts("&&&&&");
+	
+	
+	*/
+	
+	/*x = 5 ve y = 8 iken
+
+		@@@@@  cikitisi icin asagidaki kod
+
+		if (y == 8)
+			if (x == 5)
+				puts("@@@@@");
+			else
+			{
+				puts("#####");
+				puts("$$$$$");
+				puts("&&&&&");
+			}
+	*/
+
+
+	
+	/*
+	
+	x = 5 ve y = 8 iken
+
+		@@@@@
+		&&&&& cikitisi icin asagidaki kod
+	
+	if (y == 8)
+		if (x == 5)
+			puts("@@@@@");
+		else
+		{
+			puts("#####");
+			puts("$$$$$");
+		}
+			puts("&&&&&");
+			
+	*/		
+
+
+	/*
+	
+	
+	x = 5 ve y = 8 iken(sondaki 3 puts bilesik bir ifadenin parcasi)
+		#####
+		$$$$$
+		&& &&&cikitisi icin asagidaki kod
+
+
+
+	if (y == 8)
+		if (x != 5)
+			puts("@@@@@");
+		else
+		{
+			puts("#####");
+			puts("$$$$$");
+			puts("&&&&&");
+		}*/
+		
+}
