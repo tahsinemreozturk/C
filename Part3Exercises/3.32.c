2e21d869326ac66405a85d4bfda3cd5ff3636885
tahsinemreozturk <tahsinemrre@gmail.com>
2025-11-13 11:37:15 +0300

    Exercise 3.33

--- Part3Exercises/3.32.c
+++ Part3Exercises/3.32.c
@@ -1,27 +1,27 @@
-#include <stdio.h>
-
-int main()
-{
-	int kenar, i,k;
-	i = 0;
-	k = 0;
-	
-	printf("Olusturmak istediginiz karenin kenar uzunlugunu giriniz: ");
-	scanf_s("%d", &kenar);
-
-	
-	while (i < kenar)
-	{
-		k = 0; // satirlar * basilip bir bir tamamlanirken, * basilmadan atlanilmamasi icin her yeni satirda k degerine 0 atadik.
-		while (k < kenar)
-		{
-			printf("*");
-			k++;
-		}
-		printf("\n");
-		i++;
-	}
-
-	return 0;	
-
-}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int kenar, i,k;
+//	i = 0;
+//	k = 0;
+//	
+//	printf("Olusturmak istediginiz karenin kenar uzunlugunu giriniz: ");
+//	scanf_s("%d", &kenar);
+//
+//	
+//	while (i < kenar)
+//	{
+//		k = 0; // satirlar * basilip bir bir tamamlanirken, * basilmadan atlanilmamasi icin her yeni satirda k degerine 0 atadik.
+//		while (k < kenar)
+//		{
+//			printf("*");
+//			k++;
+//		}
+//		printf("\n");
+//		i++;
+//	}
+//
+//	return 0;	
+//
+//}
---
+++ Part3Exercises/3.33.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	int kenar, satir, sutun;
+	satir = 0;
+
+	printf("Ici bos kare olsuturmak icin kenar uzunlugunu giriniz: ");
+	scanf_s("%d", &kenar);
+
+	while (satir < kenar)
+	{
+		sutun = 0;
+
+		while (sutun < kenar)
+		{
+			if (satir == 0 || sutun == 0 || satir == kenar - 1 || sutun == kenar - 1  )
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+			}
+			sutun++;
+		}
+		printf("\n");
+		satir++;
+	}
+
+	return 0;
+}
