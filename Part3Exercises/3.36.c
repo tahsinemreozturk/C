dacf58e79337b6608460775e2af80dfd0550aa93
tahsinemreozturk <tahsinemrre@gmail.com>
2025-11-17 12:00:25 +0300

    Exercise 3.37

--- Part3Exercises/3.36.c
+++ Part3Exercises/3.36.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-
-int main()
-{
-	int x = 1;
-
-
-	while (x <= 1000000000)
-	{
-		if ((x % 100000000) == 0)
-		{
-			printf("%d\n",x);
-		}
-
-		x++;
-	}
-
-	return 0;
-
-}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int x = 1;
+//
+//
+//	while (x <= 1000000000)
+//	{
+//		if ((x % 100000000) == 0)
+//		{
+//			printf("%d\n",x);
+//		}
+//
+//		x++;
+//	}
+//
+//	return 0;
+//
+//}
---
+++ Part3Exercises/3.37.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 1;
+
+	while (x <= 100)
+	{
+		printf("*");
+		if (x % 10 == 0)
+		{
+			printf("\n");
+		}
+		x++;
+	}
+
+	return 0;
+}
