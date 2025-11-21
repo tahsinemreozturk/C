3c094025fd946a48a6b4e61fd38453ded05f1304
tahsinemreozturk <tahsinemrre@gmail.com>
2025-11-21 10:23:45 +0300

    Exercise 3.41

---
+++ Part3Exercises/3.41.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	float yaricap, pi,cap,alan,cevre;
+	pi = 3.14159;
+
+	printf("Yaricapi Girilen Cemberin Capi, Alani ve Cevresini Hesaplayan Program\n");
+
+	printf("Yaricap degerini giriniz: ");
+	scanf_s("%f", &yaricap);
+
+
+	cap = yaricap * 2;
+	cevre = 2 * pi * yaricap;
+	alan = pi * yaricap * yaricap;
+
+	printf("Cemberin Capi: %.4f Cevresi: %.4f Alani: %.4f", cap, cevre, alan);
+
+	return 0;
+	
+}
