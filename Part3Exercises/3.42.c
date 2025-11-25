30f9c3df17f9474a4b4fa155dd9c7b361ae38feb
tahsinemreozturk <tahsinemrre@gmail.com>
2025-11-25 08:26:45 +0300

    Exercise 3.44

--- Part3Exercises/3.42.c
+++ Part3Exercises/3.42.c
@@ -1,22 +1,23 @@
-﻿#include <stdio.h>
-
-int main()
-{
-	int x = 1;
-	int y = 2;
-	
-	//printf("%d", ++(x + y)); --> HATALI
-
-	int z = x + y;
-	printf("%d", ++z); // ----> DOGRU KULLANIM
-
-}
-//Burada ++(x+y) ifadesi hatalidir. Cunku ++ operatoru degistirilebilir 
-// (lvalue) ifadelerle kullanilabilir, ancak (x + y) ifadesi degistirilebilir degildir.
-// Bu nedenle, derleyici bu satiri isleme alirken hata verecektir.
-// Bu kodu duzeltmek icin, degistirilebilir bir ifade kullanmak gerekir.
-// 
-
-// ++x;      gecerli
-// ++(x);   gecerli
-// ++(x + y);  GECERSIZ → cunku x+y bir degisken degil, HESAPLANMIS bir degerdir
+﻿//#include <stdio.h>
+//
+//int main()
+//{
+//	int x = 1;
+//	int y = 2;
+//	
+//	//printf("%d", ++(x + y)); --> HATALI
+//
+//	int z = x + y;
+//	printf("%d", ++z); // ----> DOGRU KULLANIM
+//
+//}
+////Burada ++(x+y) ifadesi hatalidir. Cunku ++ operatoru degistirilebilir 
+//// (lvalue) ifadelerle kullanilabilir, ancak (x + y) ifadesi degistirilebilir degildir.
+//// Bu nedenle, derleyici bu satiri isleme alirken hata verecektir.
+//// Bu kodu duzeltmek icin, degistirilebilir bir ifade kullanmak gerekir.
+//// 
+//
+//// ++x;      gecerli
+//// ++(x);   gecerli
+//// ++(x + y);  GECERSIZ → cunku x+y bir degisken degil, HESAPLANMIS bir degerdir
+//
