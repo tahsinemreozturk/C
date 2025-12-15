2bd7012c04961256f39122d26c5fd6c5de83a9d8
tahsinemreozturk <tahsinemrre@gmail.com>
2025-12-15 09:25:13 +0300

    Exercise 4.7

---
+++ Part4Exercies/4.7.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+
+
+	// 1,2,3,4,5,6,7 ciktisini veren for dongusunu yaziniz.
+
+	for (int x = 1; x <= 7; x++)
+	{
+		printf("%d ", x);
+	}
+
+	printf("\n");
+	// 3 8 13 18 23 ciktisini veren for dongusunu yaziniz.
+	for (int y = 3; y <= 23; y += 5)
+	{
+		printf("%d ", y);
+	}
+
+	printf("\n");
+	// 20 14 8 2 -4 -10 ciktisini veren for dongusunu yaziniz.
+	for (int z = 20; z >= -10; z -= 6)
+	{
+		printf("%d ", z);
+
+	}
+
+	printf("\n");
+	//19 27 35 43 51 ciktisini veren for dongusunu yaziniz.
+	for (int k = 19; k <= 51; k += 8)
+	{
+		printf("%d ", k);
+	}
+
+
+	return 0;
+
+}
