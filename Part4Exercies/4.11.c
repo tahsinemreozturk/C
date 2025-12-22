c5487a5c93a5e8040118885c0888d5b371267040
tahsinemreozturk <tahsinemrre@gmail.com>
2025-12-22 09:41:08 +0300

    Exercise 4.12

--- Part4Exercies/4.11.c
+++ Part4Exercies/4.11.c
@@ -1,36 +1,36 @@
-#include <stdio.h>
-
-int main()
-{
-	int count;
-	int value;
-	int minValue;
-
-	printf("How many numbers will you enter? ");
-
-	if (scanf_s("%d", &count) != 1 || count <= 0)
-	{
-		printf("Invalid input.\n");
-		return 1;
-	}
-
-	printf("Enter number 1: ");
-	scanf_s("%d", &value);
-	minValue = value;
-
-
-	for (int i = 2; i <= count; i++)
-	{
-		printf("Enter number %d: ", i);
-		scanf_s("%d", &value);
-
-		if (value < minValue)
-		{
-			minValue = value;
-		}
-	}
-
-	printf("The smallest number is: %d\n", minValue);
-	return 0;
-
-}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int count;
+//	int value;
+//	int minValue;
+//
+//	printf("How many numbers will you enter? ");
+//
+//	if (scanf_s("%d", &count) != 1 || count <= 0)
+//	{
+//		printf("Invalid input.\n");
+//		return 1;
+//	}
+//
+//	printf("Enter number 1: ");
+//	scanf_s("%d", &value);
+//	minValue = value;
+//
+//
+//	for (int i = 2; i <= count; i++)
+//	{
+//		printf("Enter number %d: ", i);
+//		scanf_s("%d", &value);
+//
+//		if (value < minValue)
+//		{
+//			minValue = value;
+//		}
+//	}
+//
+//	printf("The smallest number is: %d\n", minValue);
+//	return 0;
+//
+//}
---
+++ Part4Exercies/4.12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	int sum = 0;
+
+	for (int x =2; x <=30; x++)
+	{
+		if (x % 2 == 0)
+		{
+			sum += x;
+		}
+	}
+
+	printf("Sum of even numbers from 2 to 30: %d\n", sum);
+	return 0;
+}
