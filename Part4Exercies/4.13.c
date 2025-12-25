77bd1fc15b739c9ac274cb7b06b21bf05b08ad6c
tahsinemreozturk <tahsinemrre@gmail.com>
2025-12-25 11:11:05 +0300

    Exercise 4.14

--- Part4Exercies/4.13.c
+++ Part4Exercies/4.13.c
@@ -1,41 +1,41 @@
-
-// HATALI KOD
-
-/* #include <stdio.h>
-
-int main()
-{
-	int x = 1;
-
-	for (int a = 1; a <= 15; a++) {
-		x++;
-		if (x % 2 != 0 )
-		{
-			int temp = 1;
-			int carpim = 1;
-			carpim = carpim * temp * x;
-		}
-	}
-}
-*/
-
-// DOGRU KOD
- 
-#include <stdio.h>
-
-int main()
-{
-	int carpim = 1;
-
-	for (int i = 1; i <= 15; i++)
-	{
-		if (i % 2 != 0)   // tek sayý kontrolü
-		{
-			carpim *= i;
-		}
-	}
-
-	printf("1'den 15'e kadar tek sayilarin carpimi: %d\n", carpim);
-
-	return 0;
-}
+//
+//// HATALI KOD
+//
+///* #include <stdio.h>
+//
+//int main()
+//{
+//	int x = 1;
+//
+//	for (int a = 1; a <= 15; a++) {
+//		x++;
+//		if (x % 2 != 0 )
+//		{
+//			int temp = 1;
+//			int carpim = 1;
+//			carpim = carpim * temp * x;
+//		}
+//	}
+//}
+//*/
+//
+//// DOGRU KOD
+// 
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int carpim = 1;
+//
+//	for (int i = 1; i <= 15; i++)
+//	{
+//		if (i % 2 != 0)   // tek sayý kontrolü
+//		{
+//			carpim *= i;
+//		}
+//	}
+//
+//	printf("1'den 15'e kadar tek sayilarin carpimi: %d\n", carpim);
+//
+//	return 0;
+//}
