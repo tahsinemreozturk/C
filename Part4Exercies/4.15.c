0f05a611b80911dc3879b698f07aaf1a7bc36a54
tahsinemreozturk <tahsinemrre@gmail.com>
2025-12-27 10:58:49 +0300

    Exercise 4.16

--- Part4Exercies/4.15.c
+++ Part4Exercies/4.15.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
-#include <math.h>
-
-int main(void)
-{
-	double amount;
-	double principal = 1000.00;
-	double rate ;
-	unsigned int year;
-
-	
-	printf("%4s%10s%21s\n", "Year", "Rate", " Amount on deposit");
-	printf("-------- -------- ---------------------------\n");
-
-	for (year = 1; year <= 10; ++year)
-	{
-		for (rate = 0.05; rate <= 0.10; rate += 0.01)
-		{
-			amount = principal * pow(1.0 + rate, year);
-			// rate * 100 ekleyerek hangi faiz oraný olduðunu gösterdik
-			printf("%4u%9.0f%%%21.2f\n", year, rate * 100, amount);
-		}
-		puts("");
-	}
-}
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main(void)
+//{
+//	double amount;
+//	double principal = 1000.00;
+//	double rate ;
+//	unsigned int year;
+//
+//	
+//	printf("%4s%10s%21s\n", "Year", "Rate", " Amount on deposit");
+//	printf("-------- -------- ---------------------------\n");
+//
+//	for (year = 1; year <= 10; ++year)
+//	{
+//		for (rate = 0.05; rate <= 0.10; rate += 0.01)
+//		{
+//			amount = principal * pow(1.0 + rate, year);
+//			// rate * 100 ekleyerek hangi faiz oraný olduðunu gösterdik
+//			printf("%4u%9.0f%%%21.2f\n", year, rate * 100, amount);
+//		}
+//		puts("");
+//	}
+//}
---
+++ Part4Exercies/4.16.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main()
+
+{
+	int i, j;
+	for (i = 0; i< 10; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			printf("%s", "*");
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+
+	int k, l;
+	for (k = 10; k >= 1; k--)
+	{
+		for (l = 0; l < k; l++)
+		{
+			printf("%s", "*");
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+
+	int a, b, c;
+
+	for (a = 10; a >= 1; a--)
+	{
+		// boþluklar
+		for (b = 0; b < 10 - a; b++)
+		{
+			printf("%s", " ");
+		}
+
+		// yýldýzlar
+		for (c = 0; c < a; c++)
+		{
+			printf("%s", "*");
+		}
+
+		printf("\n");
+	}
+
+	printf("\n");
+
+	int m, p, n;
+
+	for (m = 1; m <= 10; m++)
+	{
+		for (p = 0; p < 10 - m; p++) {
+			printf("%s", " ");
+		}
+
+		for (n = 0; n < m; n++)
+		{
+			printf("%s", "*");
+		}
+
+		printf("\n");
+	}
+}
