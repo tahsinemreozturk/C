b9f95136f3d3c9d3b0623585c2b98552162c0cd8
tahsinemreozturk <tahsinemrre@gmail.com>
2026-01-01 11:16:18 +0300

    Exercise 4.18

---
+++ Part4Exercies/4.18.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c,d,e;
+	
+	printf("Enter a number between 5-30: ");
+	scanf("%d", &a);
+
+	printf("Enter a number between 5-30: ");
+	scanf("%d", &b);
+
+	printf("Enter a number between 5-30: ");
+	scanf("%d", &c);
+
+	printf("Enter a number between 5-30: ");
+	scanf("%d", &d);
+
+	printf("Enter a number between 5-30: ");
+	scanf("%d", &e);
+	
+	
+
+	for(int x = 0; x < a; x++)
+	{
+		printf("*");
+	}
+	printf("\n");
+
+	for (int y = 0; y < b; y++)
+	{
+		printf("*");
+	}
+	printf("\n");
+
+	for (int z = 0; z < c; z++)
+	{
+		printf("*");
+	}
+	printf("\n");
+
+	for (int k = 0; k < d; k++)
+	{
+		printf("*");
+	}
+	printf("\n");
+
+	for (int l = 0; l < e; l++)
+	{
+		printf("*");
+	}
+
+
+	return 0;
+}
