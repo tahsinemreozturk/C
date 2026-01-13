6c8beab685aee8254d42ab8c6675a268d7fe3380
tahsinemreozturk <tahsinemrre@gmail.com>
2026-01-13 18:45:11 +0300

    Exercise 4.27

--- Part4Exercies/4.26.c
+++ Part4Exercies/4.26.c
@@ -1,33 +1,33 @@
-#include <stdio.h>
-
-int main()
-{
-	int number;
-
-	printf("Enter a number: ");
-	scanf("%d", &number);
-
-	printf("\n");
-
-	for (int i = 1; i <= number; i++)
-	{
-		for (int b = 1; b <= i; b++)
-		{
-			printf("%d ", i);
-		}
-		printf("\n");
-	}
-
-	printf("\n");
-
-	for (int a = 1; a <= number; a++)
-	{
-		for (int c = number; c >= a; c--)
-		{
-			printf("%d ", a);
-		}
-		printf("\n");
-	}
-
-	printf("\n");
-}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number;
+//
+//	printf("Enter a number: ");
+//	scanf("%d", &number);
+//
+//	printf("\n");
+//
+//	for (int i = 1; i <= number; i++)
+//	{
+//		for (int b = 1; b <= i; b++)
+//		{
+//			printf("%d ", i);
+//		}
+//		printf("\n");
+//	}
+//
+//	printf("\n");
+//
+//	for (int a = 1; a <= number; a++)
+//	{
+//		for (int c = number; c >= a; c--)
+//		{
+//			printf("%d ", a);
+//		}
+//		printf("\n");
+//	}
+//
+//	printf("\n");
+//}
