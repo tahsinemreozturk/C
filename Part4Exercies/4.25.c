f7c2cad06decfca1dbf37f660ca7b8d28467ade2
tahsinemreozturk <tahsinemrre@gmail.com>
2026-01-13 18:42:56 +0300

    Exercise 4.26

--- Part4Exercies/4.25.c
+++ Part4Exercies/4.25.c
@@ -1,34 +1,34 @@
-#include <stdio.h>
-
-int main() {
-	int num, first, second, third;
-
-	first = 1;
-	second = 1;
-	third = 1;
-
-
-	printf("Enter a number: ");
-	scanf("%d", &num);
-
-	if (num == 1)
-	{
-		printf("1 ");
-	}
-
-	else if ( num >= 2)
-	{
-		printf("1 1 ");
-	}
-
-	for (int i = 1; i < num - 1; i++)
-	{
-		first = second;
-		second = third;
-		third = first + second;
-
-		printf("%d ", third);
-	}
-
-	
-}
+//#include <stdio.h>
+//
+//int main() {
+//	int num, first, second, third;
+//
+//	first = 1;
+//	second = 1;
+//	third = 1;
+//
+//
+//	printf("Enter a number: ");
+//	scanf("%d", &num);
+//
+//	if (num == 1)
+//	{
+//		printf("1 ");
+//	}
+//
+//	else if ( num >= 2)
+//	{
+//		printf("1 1 ");
+//	}
+//
+//	for (int i = 1; i < num - 1; i++)
+//	{
+//		first = second;
+//		second = third;
+//		third = first + second;
+//
+//		printf("%d ", third);
+//	}
+//
+//	
+//}
