e3dea2fb73bb24e805d35bbefc2593d3030d7c5f
tahsinemreozturk <tahsinemrre@gmail.com>
2026-01-15 13:36:35 +0300

    Exercise 4.28

--- Part4Exercies/4.27.c
+++ Part4Exercies/4.27.c
@@ -1,40 +1,40 @@
-#include <stdio.h>
-
-int main()
-{
-	int number;
-
-	printf("Enter a number: ");
-	scanf("%d", &number);
-
-	printf("\n\n");
-
-	for (int k = 0; k < number; k++)
-	{
-		printf("* ");
-	}
-	printf("\n");
-
-	for (int u = 0; u < number - 2; u++)
-	{
-		for (int l = 0; l < number; l++)
-		{
-			if (l == 0 || l == number - 1)
-			{
-				printf("* ");
-			}
-			else
-			{
-				printf("  ");
-			}
-
-		}
-		printf("\n");
-	}
-
-	for (int m = 0; m < number; m++)
-	{
-		printf("* ");
-	}
-	printf("\n");
-}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number;
+//
+//	printf("Enter a number: ");
+//	scanf("%d", &number);
+//
+//	printf("\n\n");
+//
+//	for (int k = 0; k < number; k++)
+//	{
+//		printf("* ");
+//	}
+//	printf("\n");
+//
+//	for (int u = 0; u < number - 2; u++)
+//	{
+//		for (int l = 0; l < number; l++)
+//		{
+//			if (l == 0 || l == number - 1)
+//			{
+//				printf("* ");
+//			}
+//			else
+//			{
+//				printf("  ");
+//			}
+//
+//		}
+//		printf("\n");
+//	}
+//
+//	for (int m = 0; m < number; m++)
+//	{
+//		printf("* ");
+//	}
+//	printf("\n");
+//}
