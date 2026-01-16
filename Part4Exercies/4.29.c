6d6ca2989d6e80e82d69f2db9ff8ddbcc8ad28b1
tahsinemreozturk <tahsinemrre@gmail.com>
2026-01-16 09:18:55 +0300

    Exercise 4.30

--- Part4Exercies/4.29.c
+++ Part4Exercies/4.29.c
@@ -1,30 +1,30 @@
-#include <stdio.h>
-
-int main()
-{
-	int i, j, number;
-
-	printf("Enter an integer: ");
-	scanf("%d", &number);
-
-
-	for (i = 0; i < number; i++)
-	{
-
-		for (j = 0; j < i; j++)
-		{
-			printf(" ");
-		}
-
-
-		for (j = 0; j < number - i ; j++)
-		{
-			printf("* ");
-		}
-		printf("\n");
-
-	}
-
-	return 0;
-}
-
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i, j, number;
+//
+//	printf("Enter an integer: ");
+//	scanf("%d", &number);
+//
+//
+//	for (i = 0; i < number; i++)
+//	{
+//
+//		for (j = 0; j < i; j++)
+//		{
+//			printf(" ");
+//		}
+//
+//
+//		for (j = 0; j < number - i ; j++)
+//		{
+//			printf("* ");
+//		}
+//		printf("\n");
+//
+//	}
+//
+//	return 0;
+//}
+//
