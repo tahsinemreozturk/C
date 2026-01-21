bd0a730f4bee0f0e0dafff02e39d298bc2d95e6b
tahsinemreozturk <tahsinemrre@gmail.com>
2026-01-21 13:22:48 +0300

    Exercise 4.31

--- Part4Exercies/4.30.c
+++ Part4Exercies/4.30.c
@@ -1,45 +1,45 @@
-#include <stdio.h>
-
-int main()
-{
-	int i, j, number;
-
-	printf("Enter a integer: ");
-	scanf("%d", &number);
-
-	for (i = 1; i <= number; i++)
-	{
-		for (j = 0; j < number - i; j++)
-		{
-			printf(" ");
-		}
-
-		for (j = 0; j < i; j++)
-		{
-			printf("* ");
-		}
-
-		printf("\n");
-
-	}
-
-	for (i = 0; i < number; i++)
-	{
-
-		for (j = 0; j <= i; j++)
-		{
-			printf(" ");
-		}
-
-
-		for (j = 1; j < number - i; j++)
-		{
-			printf("* ");
-		}
-		printf("\n");
-
-	}
-
-	return 0;
-}
-
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i, j, number;
+//
+//	printf("Enter a integer: ");
+//	scanf("%d", &number);
+//
+//	for (i = 1; i <= number; i++)
+//	{
+//		for (j = 0; j < number - i; j++)
+//		{
+//			printf(" ");
+//		}
+//
+//		for (j = 0; j < i; j++)
+//		{
+//			printf("* ");
+//		}
+//
+//		printf("\n");
+//
+//	}
+//
+//	for (i = 0; i < number; i++)
+//	{
+//
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf(" ");
+//		}
+//
+//
+//		for (j = 1; j < number - i; j++)
+//		{
+//			printf("* ");
+//		}
+//		printf("\n");
+//
+//	}
+//
+//	return 0;
+//}
+//
