f519feadcc469a46f3aa3c46803f8fe04a7a2310
tahsinemreozturk <tahsinemrre@gmail.com>
2026-01-24 09:54:20 +0300

    Exercise 4.32

--- Part4Exercies/4.31.c
+++ Part4Exercies/4.31.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
-
-int main()
-{
-	int i, j;
-	char input, character = 'A';
-
-	printf("Enter an uppercase letter: ");
-	scanf("%c", &input);
-
-	for (i = 0; i < input - 'A' + 1; i++)
-	{
-		for (j = 0; j <= i; j++)
-		{
-			printf("%c", character);
-		}
-		character++;
-		printf("\n");
-	}
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i, j;
+//	char input, character = 'A';
+//
+//	printf("Enter an uppercase letter: ");
+//	scanf("%c", &input);
+//
+//	for (i = 0; i < input - 'A' + 1; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("%c", character);
+//		}
+//		character++;
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
