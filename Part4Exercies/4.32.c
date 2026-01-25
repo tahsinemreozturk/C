9a0a11136a915916d753d3e645ed4db253423d41
tahsinemreozturk <tahsinemrre@gmail.com>
2026-01-25 12:52:57 +0300

    Exercise 4.33

--- Part4Exercies/4.32.c
+++ Part4Exercies/4.32.c
@@ -1,29 +1,29 @@
-#include <stdio.h>
-
-int main()
-{
-	int space, i, rows, j, number = 1;
-
-	printf("Enter number of rows: ");
-	scanf("%d", &rows);
-
-	for (i = 0; i < rows; i++)
-	{
-		for (space = 1; space <= rows - i; space++)
-			printf("  ");
-		for (j = 0; j <= i; j++)
-		{
-			if (j == 0 || i == 0)
-				number = 1;
-			else
-				number = number * (i - j + 1) / j;
-
-			printf("%4d", number);
-		}
-
-		printf("\n");
-	}
-
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int space, i, rows, j, number = 1;
+//
+//	printf("Enter number of rows: ");
+//	scanf("%d", &rows);
+//
+//	for (i = 0; i < rows; i++)
+//	{
+//		for (space = 1; space <= rows - i; space++)
+//			printf("  ");
+//		for (j = 0; j <= i; j++)
+//		{
+//			if (j == 0 || i == 0)
+//				number = 1;
+//			else
+//				number = number * (i - j + 1) / j;
+//
+//			printf("%4d", number);
+//		}
+//
+//		printf("\n");
+//	}
+//
+//
+//	return 0;
+//}
