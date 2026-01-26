124fb224d8d7d486e0c0a5b2ed801ec0c806b097
tahsinemreozturk <tahsinemrre@gmail.com>
2026-01-26 13:44:58 +0300

    Exercise 4.34

--- Part4Exercies/4.33.c
+++ Part4Exercies/4.33.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
-
-int main()
-{
-	int i, j, row, a = 0;
-	char myLetter;
-
-	printf("Enter a letter: ");
-	scanf("%c", &myLetter);
-	printf("Please enter the number of row (odd number) :");
-	scanf("%d", &row);
-
-	for (i = 0; i < row; i++)
-	{
-		if(i<=row/2)
-			a++;
-		else
-			a--;
-		for(j=1;j<a;j++)
-			printf(" ");
-
-		printf("%c\n", myLetter);
-	}
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i, j, row, a = 0;
+//	char myLetter;
+//
+//	printf("Enter a letter: ");
+//	scanf("%c", &myLetter);
+//	printf("Please enter the number of row (odd number) :");
+//	scanf("%d", &row);
+//
+//	for (i = 0; i < row; i++)
+//	{
+//		if(i<=row/2)
+//			a++;
+//		else
+//			a--;
+//		for(j=1;j<a;j++)
+//			printf(" ");
+//
+//		printf("%c\n", myLetter);
+//	}
+//	return 0;
+//}
---
+++ Part4Exercies/4.34.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+	int i, j, obeb = 0, okek = 0, smallestNumber;
+
+	printf("OBEB'i ve OKEK'i bulunacak iki sayi girin: ");
+	scanf("%d %d", &i, &j);
+
+	if(i<j)
+		smallestNumber = i;
+	else
+		smallestNumber = j;
+
+	for (int k = 2; k <= smallestNumber; k++)
+	{
+		if ((i % k == 0) && (j % k == 0))
+		{
+			obeb = k;
+		}
+
+	}
+	if (obeb != 0)
+	{
+		okek = obeb * i / obeb * j / obeb;
+		printf("%d ve %d sayilarinin OBEB: %d\nOKEK: %d\n",i,j ,obeb, okek);
+	}
+	else
+	{
+		okek = i * j;
+		printf("%d ve %d sayilarinin OBEB: %d\nOKEK: %d\n", i, j, obeb, okek);
+	}
+		
+	return 0;
+}
+
