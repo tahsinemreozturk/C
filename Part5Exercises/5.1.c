cae0bd8fe5612dd48b7b13f3f8e78974f931b021
tahsinemreozturk <tahsinemrre@gmail.com>
2026-02-11 19:55:48 +0300

    Exercise 5.1 (Topic of functions.)

---
+++ Part5Exercises/5.1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+void myMessage()
+{
+	printf("Now i can write a function\n");
+}
+
+void nameList(char name[], int age)
+{
+	printf("%s You are %d years old\n", name, age);
+}
+
+
+int main()
+{
+	myMessage();
+	nameList("Emre", 30);
+	nameList("Alice", 25);
+	nameList("Tejo", 35);
+
+	return 0;
+}
